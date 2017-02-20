4e7d5d4ce27204099829b29d17a82e8d8e0b5b83
Nicholas Sinotte <nsinotte@proshipinc.com>
2017-02-20 17:22:20 -0600

    cleaned up FileProcessor.cs by replacing bad script with memory references for quicker loading.

--- SkylineChallenges-CSharp/FileProcessingRefactor/FileProcessor.cs
+++ SkylineChallenges-CSharp/FileProcessingRefactor/FileProcessor.cs
@@ -16,91 +16,44 @@ namespace SkylineChallenges_CSharp.FileProcessingRefactor
             for (int i = 1; i < lines.Length; i++)
             {
                 User user = new User();
-
-                string line = lines[i];
-                string[] attributes = line.Split(',');
+                Color color = new Color();
+                string[] attributes = lines[i].Split(',');
                 
+                //Setting basic values
                 user.Id = new Guid(attributes[0]);
                 user.FirstName = attributes[1];
                 user.LastName = attributes[2];
                 user.Birthday = DateTime.Parse(attributes[3]);
                 user.Email = attributes[5];
 
-                //Generations based on https://www.careerplanner.com/Career-Articles/Generations.cfm
-                string birthdayString = attributes[3];
-                DateTime birthday = DateTime.Parse(birthdayString);
-                string generation = "Unknown";
-                if (birthday >= DateTime.Parse("1910-01-01") && birthday <= DateTime.Parse("1924-12-31 23:59:59"))
-                {
-                    generation = "The Greatest Generation";
-                }
-                else if (birthday >= DateTime.Parse("1925-01-01") && birthday <= DateTime.Parse("1945-12-31 23:59:59"))
-                {
-                    generation = "The Silent Generation";
-                }
-                else if (birthday >= DateTime.Parse("1946-01-01") && birthday <= DateTime.Parse("1964-12-31 23:59:59"))
-                {
-                    generation = "Baby Boomers";
-                }
-                else if (birthday >= DateTime.Parse("1965-01-01") && birthday <= DateTime.Parse("1979-12-31 23:59:59"))
-                {
-                    generation = "Gen X";
-                }
-                else if (birthday >= DateTime.Parse("1980-01-01") && birthday <= DateTime.Parse("1995-12-31 23:59:59"))
-                {
-                    generation = "Millenials";
-                }
-                else if (birthday >= DateTime.Parse("1996-01-01") && birthday <= DateTime.Parse("2010-12-31 23:59:59"))
-                {
-                    generation = "Generation Z";
-                }
+                //Set Generations based on https://www.careerplanner.com/Career-Articles/Generations.cfm
+                if (user.Birthday >= DateTime.Parse("1910-01-01") && user.Birthday <= DateTime.Parse("1924-12-31 23:59:59"))
+                    user.Generation = "The Greatest Generation";
+                else if (user.Birthday >= DateTime.Parse("1925-01-01") && user.Birthday <= DateTime.Parse("1945-12-31 23:59:59"))
+                    user.Generation = "The Silent Generation";
+                else if (user.Birthday >= DateTime.Parse("1946-01-01") && user.Birthday <= DateTime.Parse("1964-12-31 23:59:59"))
+                    user.Generation = "Baby Boomers";
+                else if (user.Birthday >= DateTime.Parse("1965-01-01") && user.Birthday <= DateTime.Parse("1979-12-31 23:59:59"))
+                    user.Generation = "Gen X";
+                else if (user.Birthday >= DateTime.Parse("1980-01-01") && user.Birthday <= DateTime.Parse("1995-12-31 23:59:59"))
+                    user.Generation = "Millenials";
+                else if (user.Birthday >= DateTime.Parse("1996-01-01") && user.Birthday <= DateTime.Parse("2010-12-31 23:59:59"))
+                    user.Generation = "Generation Z";
                 else
-                {
-                    generation = "Unknown";
-                }
-
-                user.Generation = generation;
-
-                string hexValue = attributes[4];
-                hexValue = hexValue.Replace("#", "");
-                string redHex = hexValue[0].ToString() + hexValue[1].ToString();
-                string greenHex = hexValue[2].ToString() + hexValue[3].ToString();
-                string blueHex = hexValue[4].ToString() + hexValue[5].ToString();
-                int red = HexToDecimal(redHex);
-                int green = HexToDecimal(greenHex);
-                int blue = HexToDecimal(blueHex);
-
-                Color color = new Color();
-                color.Red = red;
-                color.Green = green;
-                color.Blue = blue;
+                    user.Generation = "Unknown";
+                
+                //Set Color based on the hex value
+                string hexValue = attributes[4].Substring(1);
+                color.Red = HexToDecimal(hexValue[0].ToString() + hexValue[1].ToString());
+                color.Green = HexToDecimal(hexValue[2].ToString() + hexValue[3].ToString());
+                color.Blue = HexToDecimal(hexValue[4].ToString() + hexValue[5].ToString());
                 user.ProfileColor = color;
 
+                //Getting credit card information
                 string creditCardInfo;
                 string creditCardNumberString = attributes[6];
                 string creditCardType = attributes[7] != null ? attributes[7].Replace("\r", "") : "";
-
-                if (creditCardType.Equals("Visa"))
-                {
-                    creditCardInfo = "V";
-                }
-                else if (creditCardType.Equals("MasterCard"))
-                {
-                    creditCardInfo = "MC";
-                }
-                else if (creditCardType.Equals("Discover"))
-                {
-                    creditCardInfo = "D";
-                }
-                else if (creditCardType.Equals("American Express"))
-                {
-                    creditCardInfo = "AMEX";
-                }
-                else
-                {
-                    creditCardInfo = "O";
-                }
-
+                
                 switch(creditCardType)
                 {
                     case "Visa":
@@ -119,17 +72,14 @@ namespace SkylineChallenges_CSharp.FileProcessingRefactor
                         creditCardInfo = "O";
                         break;
                 }   
-
-                if (creditCardNumberString != null && creditCardNumberString.Equals("") == false)
+                
+                if (!string.IsNullOrEmpty(creditCardNumberString))
                 {
-                    int creditCardNumberLength = creditCardNumberString.Length;
-
-                    creditCardInfo = creditCardInfo + creditCardNumberString.Substring(creditCardNumberLength - 4);
+                    creditCardInfo = creditCardInfo + creditCardNumberString.Substring(creditCardNumberString.Length - 4);
                     user.CreditCardInfo = creditCardInfo;
                 }
 
                 users.Add(user);
-               
             }
 
             return users;
@@ -141,16 +91,12 @@ namespace SkylineChallenges_CSharp.FileProcessingRefactor
         /// <param name="hex">Two character Hex that will be converted</param>
         /// <returns>hexidecimal value of the sum of both characters</returns>
         public int HexToDecimal(string hex)
-        {
-            string[] digits = new string[2];
-            digits[0] = hex[0].ToString();
-            digits[1] = hex[1].ToString();
-            
-            int digitA = SingleHexValue(digits[0]);
-            if (digitA == 0) digitA = int.Parse(digits[0]);
+        {            
+            int digitA = SingleHexValue(hex[0]);
+            if (digitA == 0) digitA = int.Parse(hex[0].ToString());
 
-            int digitB = SingleHexValue(digits[1]);
-            if (digitB == 0) digitB = int.Parse(digits[1]);
+            int digitB = SingleHexValue(hex[1]);
+            if (digitB == 0) digitB = int.Parse(hex[1].ToString());
 
             return digitA*16 + digitB;
         }
@@ -160,34 +106,31 @@ namespace SkylineChallenges_CSharp.FileProcessingRefactor
         /// </summary>
         /// <param name="hex">single character string that will be converted.</param>
         /// <returns>The integer value of hex</returns>
-        private int SingleHexValue(string hex)
+        private int SingleHexValue(char hex)
         {
-            int OtherValue;
-
             //Convert character to integer
             switch (hex)
             {
-                case "A":
-                case "a":
+                case 'A':
+                case 'a':
                     return 10;
-                case "B":
-                case "b":
+                case 'B':
+                case 'b':
                     return 11;
-                case "C":
-                case "c":
+                case 'C':
+                case 'c':
                     return 12;
-                case "D":
-                case "d":
+                case 'D':
+                case 'd':
                     return 13;
-                case "E":
-                case "e":
+                case 'E':
+                case 'e':
                     return 14;
-                case "F":
-                case "f":
+                case 'F':
+                case 'f':
                     return 15;
                 default:
-                    int.TryParse(hex, out OtherValue);
-                    return OtherValue;
+                    return 0;
             }
         }
     }
--- SkylineChallenges-CSharp/FileProcessingRefactor/User.cs
+++ SkylineChallenges-CSharp/FileProcessingRefactor/User.cs
@@ -8,18 +8,7 @@ namespace SkylineChallenges_CSharp.FileProcessingRefactor
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public DateTime Birthday {
-            get
-            {
-                return this.Birthday;
-            }
-            set
-            {
-                DateTime oBirthday;
-                DateTime.TryParse(value.ToString(), out oBirthday);
-                Birthday = oBirthday;
-            }
-        }
+        public DateTime Birthday { get;set; }
         public string Generation { get; set; }
         public Color ProfileColor { get; set; }
         public string CreditCardInfo { get; set; }
